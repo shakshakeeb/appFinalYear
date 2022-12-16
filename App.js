96ccbff3d3ebd3600fc898c00d13ddf672e8665e
Shakeeb Hassan <shass006@gold.ac.uk>
2022-12-16 00:54:27 +0000

    WARN  Possible Unhandled Promise Rejection (id: 0): TypeError: Cannot read property 'fulfillmentMessages' of undefined

--- App.js
+++ App.js
@@ -1,40 +1,85 @@
-import React, {Component} from "react";
-import { View, Text, SafeAreaView} from "react-native";
+// App.js
+
+import React, { Component } from "react";
+import { StyleSheet, Text, View } from "react-native";
 import { GiftedChat } from "react-native-gifted-chat";
+import { Dialogflow_V2 } from "react-native-dialogflow";
 
-const botAvatar = require('./assets/images/robot.png')
+import { dialogflowConfig } from "./env";
 
-const BOT = {
+const botAva = require("./assets/images/robot.png");
+
+const BOT_USER = {
   _id: 2,
-  name: 'Mr. Robot',
-  avatar: botAvatar
+  name: 'FAQ Bot',
+  avatar: botAva,
 };
-
 
 class App extends Component {
   state = {
-    messages: [{_id: 1, text: 'Hi', createdAt: new Date()
-  , user: BOT}],
-    id: 1,
-    name: '',
+    messages: [
+      {
+        _id: 1,
+        text: `Hi! I am the FAQ bot 🤖 from Jscrambler.\n\nHow may I help you with today?`,
+        createdAt: new Date(),
+        user: BOT_USER
+      }
+    ]
   };
 
+  componentDidMount() {
+    Dialogflow_V2.setConfiguration(
+      dialogflowConfig.client_email,
+      dialogflowConfig.private_key,
+      Dialogflow_V2.LANG_ENGLISH_US,
+      dialogflowConfig.project_id
+    );
+  }
+
+  handleGoogleResponse(result) {
+    let text = result.queryResult.fulfillmentMessages[0].text.text[0];
+    this.sendBotResponse(text);
+  }
+
+  onSend(messages = []) {
+    this.setState(previousState => ({
+      messages: GiftedChat.append(previousState.messages, messages)
+    }));
+
+    let message = messages[0].text;
+    Dialogflow_V2.requestQuery(
+      message,
+      result => this.handleGoogleResponse(result),
+      error => console.log(error)
+    );
+  }
+
+  sendBotResponse(text) {
+    let msg = {
+      _id: this.state.messages.length + 1,
+      text,
+      createdAt: new Date(),
+      user: BOT_USER
+    };
+
+    this.setState(previousState => ({
+      messages: GiftedChat.append(previousState.messages, [msg])
+    }));
+  }
 
   render() {
     return (
-      <View style={{flex: 1, backgroundColor: "#fff"}}>
-      
-      <GiftedChat
-        messages={this.state.messages} 
-        onSend={(message) => this.onSend(message)}
-        onQuickReply={(quickReply) => this.onQuickReply
-        (quickReply)}
-        user={{_id:1}}
+      <View style={{ flex: 1, backgroundColor: '#fff' }}>
+        <GiftedChat
+          messages={this.state.messages}
+          onSend={messages => this.onSend(messages)}
+          user={{
+            _id: 1
+          }}
         />
       </View>
     );
   }
 }
-
 
 export default App;
---
+++ env.js
@@ -0,0 +1,13 @@
+export const dialogflowConfig = {
+    "type": "service_account",
+    "project_id": "chatbot-cfnu",
+    "private_key_id": "7157d4d389fdea98926cddfae2f21f2e6607814c",
+    "private_key": "-----BEGIN PRIVATE KEY-----\nMIIEvQIBADANBgkqhkiG9w0BAQEFAASCBKcwggSjAgEAAoIBAQCiWcRAxKMugVZ4\nBLbzZtI5oyCgqakN6hQISl09YNhr+ccB5F/SUJjTmVZV2W5LBOQnaoQOdcIt3yzA\nlXL4gFPU5hAZwTH3IVRbmzKKUbW19XtinIQZSplX/NJUwdyOh55mkpslr1CrdDds\nYWlToMf2CuiN+Q399Hh1f+meX4pyPwDkagTkamjHmd+nAQQAh9ZwZqMr9vccYqdR\nqJgSyiJ3relkpVpGs+frSXUGs9toTo3zA607W9RDgfdkMzob+aboyDzCRnXAU9Xq\nfN8C+TgsR/t/msI7QtG3LY97u2yheQQ28RPqC2yDslhjoo9M5PvBvH8ALEAXC8c4\nramnjk5DAgMBAAECggEADWZYVDoxTRjVa2GEPNG7PzuaE/inizj16ftCooVtnM0z\nDOfDCuemaCyV8M8Ek0I2QenrjV/7JMXF+qflzeHq9BMAYt0hw8fR9R+kS8CpEqQj\nc2+qAtu34rMWJHMTF7EqNJrGyGpQal5UvBLML+AA28C/EgdrjWQ8qay4Ol9LXNjP\nEQMdWh5rRlLpfyMPFA7JGucOJ5ZO9lNwgA8FemQxe79Ft4UfXsHcnqppbjsVH0jh\nk1whZe6wJZXfGxC+7K/ZSb9yoSjwlhgn3ngHi3PW+0tPj+bblx6khM6SAGKg3h9d\nXt7YwQFVxAn8u5Q4CRIrDQ+8PDdzCgHNu7R68e59kQKBgQDOVkgtXSxxx13X0XDU\n41vmWQp9tJBD8cgiGccL5WXi9Eav55gy8dyuH3YppL7/9+Zg+oVeU9yTatIX+gwk\nQOLlx/r/rnwuW6FbDGBfAOiM5joIPmnWqzysv0roDHflcGhRlBks2K/uEObFLUyn\nC+yAFKjEpEZw6zknrjFKjeq1UQKBgQDJbTUoZCz2tJjU6nagB+OHEkvW55V27bC/\nqM9vci5nWYfDfP7x56Noj9pDRGZs6brOtRkIJ71PpZi4gcSdLtA06shQggL5EsU9\nLcSAqeJs/CGtN1/gezJrkyU+kV1bUMNrwLBGrOzWm+MFSHrbO6cJDARzcQ9Xqh1V\nWPSxhj/1UwKBgEcHLJFxIXuJRaSlVFH0cg45WIMtRPwenuYeOYa+0PPvYuXqs1nW\nKEFIU0TCkX1LuOJpWr+93pBglTHrwYI5U+IFlUGurtDGeiT9fa82gNb/6boswgJD\nr/9HWmHG6P7yUvbCJGhrryQkbUwy69AVKrRsaCxI4Ha+gk5GiHFBqSoBAoGBAJD1\n7ETGW2mtLTRGDNs88i1aKIRgJDvz8bjtR2RAO08bqs4wndYAS6TLqluw1TnauBtd\nT8eDugWg+pkGozYv453Bvinad0NDk/lkf7/NA2xFxALCEFkgsHek7S/rM9Gs/QcG\nJJaoisYkaCx8LharBppHMMjX/B+uCPBMVcWRBWSLAoGAbtcO/dQUtufPYjNmU7Pg\nMqQRl0e0opNTv4vfrPR7zDDL03fCn4fUJryi8Ded2Tt9uSzGZ726U/jyap93mDds\nJx9URvCW/2Fw/xu77/wIUAqEDYnGfvCfHsI2iwh8kuifwsnG+T833W1qyDqpBJMx\nedhiAtzWpyAoj777w0RIRtQ=\n-----END PRIVATE KEY-----\n",
+    "client_email": "chatbot-lesson@chatbot-cfnu.iam.gserviceaccount.com",
+    "client_id": "110259830324998004965",
+    "auth_uri": "https://accounts.google.com/o/oauth2/auth",
+    "token_uri": "https://oauth2.googleapis.com/token",
+    "auth_provider_x509_cert_url": "https://www.googleapis.com/oauth2/v1/certs",
+    "client_x509_cert_url": "https://www.googleapis.com/robot/v1/metadata/x509/chatbot-lesson%40chatbot-cfnu.iam.gserviceaccount.com"
+  }
+  
